6df3574783011ff0313f714560b5efa8588df505
Dario Reis <guttyerresjunior1995@gmail.com>
2024-08-13 05:18:03 -0300

    Adicionando pasta de nodejs assicrono

---
+++ 02_nodejs-assicrono/01_entendendo-codigo-assicrono/index.js
@@ -0,0 +1,12 @@
+// Codigo normal do node.js sendo executado
+// console.log('Primeira linha');
+// console.log('Segunda linha');
+// console.log('Terceira linha');
+
+console.log('primeira linha');
+
+setTimeout(() => {
+  console.log('segunda linha');
+}, 5000);
+
+console.log('terceira linha');
